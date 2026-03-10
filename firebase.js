6326fa8cb54dd3234ef7082b883355115f9f2203
ramideilliasse-dev <ramideilliasse@gmail.com>
2026-03-10 18:10:44 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,21 @@
+ // firebase.js
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+apiKey: "AIzaSyB3rKXZjJqskewJM-cBvBRw8-ecJPvoeBw",
+authDomain: "angcomerce-v1.firebaseapp.com",
+projectId: "angcomerce-v1",
+storageBucket: "angcomerce-v1.appspot.com",
+messagingSenderId: "000000000",
+appId: "1:000000000:web:000000000"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
