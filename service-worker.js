4f49ae973e35d989a8c9532b6959c9eefbe216bd
ramideilliasse-dev <ramideilliasse@gmail.com>
2026-05-18 15:43:11 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,3 +3,10 @@
 });
 
 self.addEventListener("fetch", (e) => {});
+self.addEventListener("activate", event => {
+event.waitUntil(
+caches.keys().then(keys =>
+Promise.all(keys.map(key => caches.delete(key)))
+)
+)
+})
