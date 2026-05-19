906378b330b3774eee090792272059e09f377750
ramideilliasse-dev <ramideilliasse@gmail.com>
2026-05-19 23:47:26 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,44 @@
- self.addEventListener("install", (e) => {
-  console.log("App installée");
-});
+ const CACHE_NAME = "toma-v1"
 
-self.addEventListener("fetch", (e) => {});
-self.addEventListener("activate", event => {
+const urlsToCache = [
+
+"/",
+"/index.html",
+"/offline.html",
+"/styles.css",
+"/firebase.js",
+"/manifest.json"
+
+]
+
+self.addEventListener("install",event=>{
+
 event.waitUntil(
-caches.keys().then(keys =>
-Promise.all(keys.map(key => caches.delete(key)))
+
+caches.open(CACHE_NAME)
+.then(cache=>cache.addAll(urlsToCache))
+
 )
+
+})
+
+self.addEventListener("fetch",event=>{
+
+event.respondWith(
+
+fetch(event.request)
+
+.catch(()=>{
+
+return caches.match(event.request)
+.then(response=>{
+
+return response || caches.match("/offline.html")
+
+})
+
+})
+
 )
+
 })
