da97a3c2a921c71940ff449c0b1d96725ac82ffb
ramideilliasse-dev <ramideilliasse@gmail.com>
2026-05-26 20:28:31 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,76 @@
- const CACHE_NAME = "toma-v999";
+ const CACHE_NAME = "toma-cache-v3";
+
+const urlsToCache = [
+"/",
+"/index.html",
+"/offline.html",
+"/manifest.json"
+];
 
 self.addEventListener("install", event => {
-  self.skipWaiting();
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(urlsToCache))
+
+);
+
+self.skipWaiting();
+
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
-  );
+
+event.waitUntil(
+
+caches.keys().then(keys => {
+
+return Promise.all(
+
+keys.map(key => {
+
+if(key !== CACHE_NAME){
+return caches.delete(key);
+}
+
+})
+
+);
+
+})
+
+);
+
+self.clients.claim();
+
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+
+event.respondWith(
+
+fetch(event.request)
+
+.then(response => {
+
+const clone = response.clone();
+
+caches.open(CACHE_NAME)
+.then(cache => cache.put(event.request, clone));
+
+return response;
+
+})
+
+.catch(async () => {
+
+const cached = await caches.match(event.request);
+
+return cached || caches.match("/offline.html");
+
+})
+
+);
+
 });
