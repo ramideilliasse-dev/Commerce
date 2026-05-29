2b5e0b948fb2ee44e7e54fc5a711d4ec9c2c4579
ramideilliasse-dev <ramideilliasse@gmail.com>
2026-05-29 08:43:33 +0100

    Create upload-social-image.js

---
+++ js/upload-social-image.js
@@ -0,0 +1,44 @@
+ // ================= UPLOAD SOCIAL IMAGE =================
+
+export async function uploadSocialImage(base64){
+
+try{
+
+const formData = new FormData()
+
+formData.append(
+"file",
+base64
+)
+
+formData.append(
+"upload_preset",
+"toma_social"
+)
+
+// ================= CLOUDINARY =================
+
+const response = await fetch(
+
+"https://api.cloudinary.com/v1_1/VOTRE_CLOUD_NAME/image/upload",
+
+{
+method:"POST",
+body:formData
+}
+
+)
+
+const data = await response.json()
+
+return data.secure_url || ""
+
+}catch(e){
+
+console.error(e)
+
+return ""
+
+}
+
+}
