b1b4b3bac534fa9281159b063b37e7d99ca8bf5a
ramideilliasse-dev <ramideilliasse@gmail.com>
2026-06-10 20:49:47 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -7,24 +7,28 @@ importScripts(
 );
 
 firebase.initializeApp({
-apiKey:"AIzaSyB3rKXZjJqskewJM-cBvBRw8-ecJPvoeBw",
-authDomain:"angcomerce-v1.firebaseapp.com",
-projectId:"angcomerce-v1",
-storageBucket:"angcomerce-v1.firebasestorage.app",
-messagingSenderId:"238735890157",
-appId:"1:238735890157:web:db3f87960db7916d7fdee4"
+apiKey: "AIzaSyB3rKXZjJqskewJM-cBvBRw8-ecJPvoeBw",
+authDomain: "angcomerce-v1.firebaseapp.com",
+projectId: "angcomerce-v1",
+storageBucket: "angcomerce-v1.firebasestorage.app",
+messagingSenderId: "238735890157",
+appId: "1:238735890157:web:db3f87960db7916d7fdee4"
 });
 
-const messaging = firebase.messaging();
+const messaging =
+firebase.messaging();
 
-messaging.onBackgroundMessage((payload)=>{
+messaging.onBackgroundMessage(
+(payload)=>{
 
 self.registration.showNotification(
 payload.notification.title,
 {
-body: payload.notification.body,
-icon: "/icon-192.png"
+body:
+payload.notification.body,
+icon:"/icon-192.png"
 }
 );
 
-});
+}
+);
