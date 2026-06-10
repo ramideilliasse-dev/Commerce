4fec32f5c2ecb97767c4c8236c48700a1778b45f
ramideilliasse-dev <ramideilliasse@gmail.com>
2026-06-10 21:38:51 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,9 @@
-alert("firebase.js chargé");
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
 import {
-getMessaging,
-getToken
+getMessaging
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging.js";
-
 import {
 getAuth,
 setPersistence,
@@ -32,7 +30,7 @@ appId: "1:238735890157:web:db3f87960db7916d7fdee4"
 
 /* INIT */
 const app = initializeApp(firebaseConfig);
-alert("Firebase OK");
+
 export const messaging = getMessaging(app);
 export const auth = getAuth(app);
 
@@ -60,38 +58,6 @@ currentUser = user;
 authReady = true;
 
 console.log("🔥 Auth ready:", user?.uid);
-alert("Auth OK : " + (user?.uid || "Pas connecté"));
-if(!user) return;
-try{
-alert("Avant FCM");
-const permission =
-await Notification.requestPermission();
-alert("Permission : " + permission);
-if(permission === "granted"){
-
-const token =
-await getToken(
-messaging,
-{
-vapidKey:"BAv9JCvzV_TZ3C-rcXv6LwJL9sIzp6m-Wf0qWX6uEj33F2OVqGNBTf4E7MV1s6UbSrcyuXbIQXpZQaaduPzCPt8"
-}
-);
-alert(
-token
-? "Token FCM obtenu"
-: "Token FCM vide"
-);
-if(token){
-
-await setDoc(
-doc(db,"users",user.uid),
-{
-fcmToken: token
-},
-{
-merge:true
-}
-);
 
 console.log("✅ FCM Token:", token);
 
@@ -101,13 +67,10 @@ console.log("✅ FCM Token:", token);
 
 }catch(err){
 
-alert(
-"FCM ERROR : " +
-err.message
+console.error(
+"FCM ERROR:",
+err
 );
-
-console.error(err);
-
 }
 /* AUTO CREATE USER */
 if(!user) return;
@@ -115,12 +78,9 @@ if(!user) return;
 try{
 
 const ref = doc(db,"users",user.uid);
-alert("Lecture user Firestore");
+
  const snap = await getDoc(ref);
-alert(
-"User existe : " +
-snap.exists()
-);
+
  if(snap.exists()){
 
 const data = snap.data()
