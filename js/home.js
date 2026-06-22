e79c8802a9193b392253cd5955812128bcfce33e
ramideilliasse-dev <ramideilliasse@gmail.com>
2026-06-22 14:42:10 +0100

    Add Firebase imports and remove alert

--- js/home.js
+++ js/home.js
@@ -1,1 +1,16 @@
- alert("home.js chargé");
+ import { db, auth } from "../firebase.js";
+
+import {
+collection,
+getDocs,
+doc,
+getDoc,
+query,
+limit
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+import {
+onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+alert("Imports OK");
