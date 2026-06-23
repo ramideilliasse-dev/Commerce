73b4c5dc9042e6a58067736913beb13f00ccf890
ramideilliasse-dev <ramideilliasse@gmail.com>
2026-06-23 20:13:44 +0100

    Add product management functions in products.js

--- js/products.js
+++ js/products.js
@@ -1,1 +1,73 @@
+ // =====================================
+// PRODUCTS.JS
+// Gestion des produits
+// =====================================
 
+import {
+    getProductImage,
+    formatPrice
+} from "./ui.js";
+
+let products = [];
+
+/* ===========================
+LISTE PRODUITS
+=========================== */
+
+export function setProducts(list) {
+
+    products = list || [];
+
+}
+
+export function getProducts() {
+
+    return products;
+
+}
+
+/* ===========================
+CHERCHER PRODUIT
+=========================== */
+
+export function getProduct(id) {
+
+    return products.find(p => p.id === id);
+
+}
+
+/* ===========================
+OUVRIR PRODUIT
+=========================== */
+
+export function openProduct(id) {
+
+    localStorage.setItem(
+        "selectedProduct",
+        id
+    );
+
+    location.href =
+        "product.html?id=" + id;
+
+}
+
+/* ===========================
+IMAGE
+=========================== */
+
+export function productImage(product){
+
+    return getProductImage(product);
+
+}
+
+/* ===========================
+PRIX
+=========================== */
+
+export function productPrice(product){
+
+    return formatPrice(product.price);
+
+}
