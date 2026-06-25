ad4a075f0fe4ebcc190db9a16f37ae5ec1244f36
ramideilliasse-dev <ramideilliasse@gmail.com>
2026-06-25 22:03:02 +0100

    Set up checkout.js with imports and initial variables
    Added imports and initialized variables for checkout process.

--- js/checkout.js
+++ js/checkout.js
@@ -1,1 +1,74 @@
+ // ===============================
+// CHECKOUT.JS
+// Gestion du checkout
+// ===============================
 
+import { db, auth } from "../firebase.js";
+
+import {
+
+    collection,
+
+    addDoc,
+
+    serverTimestamp
+
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+import {
+
+    onAuthStateChanged
+
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+import {
+
+    showToast,
+
+    formatPrice
+
+} from "./ui.js";
+
+import {
+
+    getCart,
+
+    clearCart,
+
+    setProducts
+
+} from "./cart.js";
+
+/* ===============================
+   VARIABLES
+=============================== */
+
+let currentUser = null;
+
+let cart = [];
+/* ===============================
+   DOM
+=============================== */
+
+const checkoutItems =
+    document.getElementById("checkoutItems");
+
+const totalPrice =
+    document.getElementById("totalPrice");
+
+const confirmBtn =
+    document.getElementById("confirmBtn");
+/* ===============================
+   AUTH
+=============================== */
+
+onAuthStateChanged(auth,(user)=>{
+
+    currentUser = user;
+
+});
+/* ===============================
+   PANIER
+=============================== */
+
+cart = getCart();
