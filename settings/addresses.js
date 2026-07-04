eb1d0ac19c673c29b2527aa69d21c952c4676f69
ramideilliasse-dev <ramideilliasse@gmail.com>
2026-07-04 11:35:43 +0100

    Initialize addresses.js with DOM elements and imports

--- settings/addresses.js
+++ settings/addresses.js
@@ -1,1 +1,44 @@
+// ===============================
+// ADDRESSES.JS
+// Bloc 1
+// ===============================
 
+import { db } from "../firebase.js";
+
+import {
+    collection,
+    addDoc,
+    getDoc,
+    getDocs,
+    updateDoc,
+    deleteDoc,
+    doc,
+    onSnapshot,
+    writeBatch
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+import {
+    currentUser
+} from "./profile.js";
+
+import {
+    $,
+    showToast
+} from "./ui.js";
+
+let editingAddressId = null;
+
+// DOM
+const addressModal = $("addressModal");
+const addressList = $("addressList");
+
+const addAddressBtn = $("addAddressBtn");
+const saveAddressBtn = $("saveAddressBtn");
+
+const addressName = $("addressName");
+const addressPhone = $("addressPhone");
+const addressProvince = $("addressProvince");
+const addressCity = $("addressCity");
+const addressStreet = $("addressStreet");
+
+alert("✅ addresses.js Bloc 1 chargé");
