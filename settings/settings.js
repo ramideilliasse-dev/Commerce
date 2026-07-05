1a5ba7d1b7e5bd2d1c5bdbbfccfb57f5312ed0c9
ramideilliasse-dev <ramideilliasse@gmail.com>
2026-07-05 10:58:40 +0100

    Add new imports and update alert message

--- settings/settings.js
+++ settings/settings.js
@@ -2,6 +2,9 @@
 import "./ui.js";
 import "./profile.js";
 import "./merchant.js";
+import "./addresses.js";
+import "./stats.js";
+import "./security.js";
+import "./notifications.js";
 
-
-alert("5");
+alert("8");
