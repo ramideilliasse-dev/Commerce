65c1007544ab254047e24e47c6930ded0600962d
ramideilliasse-dev <ramideilliasse@gmail.com>
2026-07-05 09:29:48 +0100

    Clean up imports and modify alert in settings.js
    Removed multiple imports and updated alert message.

--- settings/settings.js
+++ settings/settings.js
@@ -1,10 +1,3 @@
  import "./events.js";
-import "./ui.js";
-import "./profile.js";
-import "./merchant.js";
-import "./addresses.js";
-import "./stats.js";
-import "./security.js";
-import "./notifications.js";
 
-alert("✅ Nouveau Settings chargé");
+alert("1");
