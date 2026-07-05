a1a13cee806b70674536add51be82c612c1df555
ramideilliasse-dev <ramideilliasse@gmail.com>
2026-07-05 11:01:32 +0100

    Clean up imports and modify alert message
    Removed unused imports and updated alert message.

--- settings/settings.js
+++ settings/settings.js
@@ -1,10 +1,5 @@
  import "./events.js";
 import "./ui.js";
 import "./profile.js";
-import "./merchant.js";
-import "./addresses.js";
-import "./stats.js";
-import "./security.js";
-import "./notifications.js";
 
-alert("8");
+alert("3");
