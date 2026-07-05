729891b6c90de60c424801acc4dd3ca0aa785f8b
ramideilliasse-dev <ramideilliasse@gmail.com>
2026-07-05 11:08:44 +0100

    Replace addresses import with stats, security, notifications

--- settings/settings.js
+++ settings/settings.js
@@ -2,6 +2,8 @@
 import "./ui.js";
 import "./profile.js";
 import "./merchant.js";
-import "./addresses.js";
+import "./stats.js";
+import "./security.js";
+import "./notifications.js";
 
-alert("5");
+alert("8");
