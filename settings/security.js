9dd5d37b0252c4dc03cd65adb7fb0e93ea2363ca
ramideilliasse-dev <ramideilliasse@gmail.com>
2026-07-06 14:05:12 +0100

    Set up security.js with initial configurations
    Initialize security.js with imports and button selections.

--- settings/security.js
+++ settings/security.js
@@ -1,1 +1,31 @@
+ // ===============================
+// SECURITY.JS
+// Partie 1
+// ===============================
 
+import { auth } from "../firebase.js";
+
+import {
+    sendEmailVerification,
+    sendPasswordResetEmail,
+    deleteUser
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+import {
+    currentUser
+} from "./profile.js";
+
+import {
+    $,
+    showToast
+} from "./ui.js";
+
+// Boutons
+
+const changePasswordBtn = $("changePasswordBtn");
+
+const verifyEmailBtn = $("verifyEmailBtn");
+
+const deleteBtn = $("deleteBtn");
+
+alert("✅ security.js Partie 1 chargée");
