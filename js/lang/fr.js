899a7f5f3f67afd409f44dd61f9742fb7cc236c6
ramideilliasse-dev <ramideilliasse@gmail.com>
2026-07-16 18:16:21 +0100

    Add French language support in fr.js

---
+++ js/lang/fr.js
@@ -0,0 +1,33 @@
+ export default {
+
+appName:"Toma",
+
+home:"Accueil",
+
+stores:"Boutiques",
+
+cart:"Panier",
+
+favorites:"Favoris",
+
+account:"Compte",
+
+recommended:"Recommandés",
+
+seeAll:"Voir tout",
+
+officialStores:"Boutiques officielles",
+
+search:"Rechercher des produits...",
+
+searchStore:"Rechercher une boutique officielle...",
+
+verified:"Vérifié",
+
+comingSoon:"Bientôt disponible",
+
+support:"Assistance",
+
+categories:"Catégories"
+
+};
