75c1eec50339358e57d6e359dc65929b9f814d28
ramideilliasse-dev <ramideilliasse@gmail.com>
2026-07-16 18:15:24 +0100

    Add Portuguese language support file

---
+++ js/lang/pt.js
@@ -0,0 +1,33 @@
+ export default {
+
+appName:"Toma",
+
+home:"Início",
+
+stores:"Lojas",
+
+cart:"Carrinho",
+
+favorites:"Favoritos",
+
+account:"Conta",
+
+recommended:"Recomendados",
+
+seeAll:"Ver tudo",
+
+officialStores:"Lojas Oficiais",
+
+search:"Pesquisar produtos...",
+
+searchStore:"Pesquisar loja oficial...",
+
+verified:"Verificado",
+
+comingSoon:"Em breve",
+
+support:"Suporte",
+
+categories:"Categorias"
+
+};
