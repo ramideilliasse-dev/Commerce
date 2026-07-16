0f20bc3346a72708c97351a488e36406853613b6
ramideilliasse-dev <ramideilliasse@gmail.com>
2026-07-16 18:16:55 +0100

    Add English language localization file

---
+++ js/lang/en.js
@@ -0,0 +1,33 @@
+ export default {
+
+appName:"Toma",
+
+home:"Home",
+
+stores:"Stores",
+
+cart:"Cart",
+
+favorites:"Favorites",
+
+account:"Account",
+
+recommended:"Recommended",
+
+seeAll:"See all",
+
+officialStores:"Official Stores",
+
+search:"Search products...",
+
+searchStore:"Search official store...",
+
+verified:"Verified",
+
+comingSoon:"Coming Soon",
+
+support:"Support",
+
+categories:"Categories"
+
+};
