029197a154ebc41edfb249eb53cfeff5ecaa406f
ramideilliasse-dev <ramideilliasse@gmail.com>
2026-07-17 21:33:58 +0100

    Refactor import statements for i18n module

--- js/language.js
+++ js/language.js
@@ -1,13 +1,11 @@
- import { setLanguage } from "./lang/i18n.js";
-import { translatePage } from "./lang/i18n.js";
+ import { setLanguage, translatePage } from "./lang/i18n.js";
+
 translatePage();
 
 document.querySelectorAll(".languageCard").forEach(card => {
 
     card.onclick = () => {
 
-        alert("Langue choisie : " + card.dataset.lang);
-alert(localStorage.getItem("toma_language"));
         setLanguage(card.dataset.lang);
 
     };
