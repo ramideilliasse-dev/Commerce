bc3554ac07de473b90e5a022f60179e542b45c63
ramideilliasse-dev <ramideilliasse@gmail.com>
2026-08-04 20:21:14 +0100

    Add store details for various brands

--- js/official-store.js
+++ js/official-store.js
@@ -28,91 +28,118 @@ BOUTIQUES OFFICIELLES
 const stores = {
 
 apple:{
+id:"apple",
 name:"Apple",
+category:"Loja Oficial",
 logo:"images/stores/apple.png",
 banner:"images/stores/apple-banner.jpg"
 },
 
 samsung:{
+id:"samsung",
 name:"Samsung",
+category:"Loja Oficial",
 logo:"images/stores/samsung.png",
 banner:"images/stores/samsung-banner.jpg"
 },
 
 xiaomi:{
+id:"xiaomi",
 name:"Xiaomi",
+category:"Loja Oficial",
 logo:"images/stores/xiaomi.png",
 banner:"images/stores/xiaomi-banner.jpg"
 },
 
 huawei:{
+id:"huawei",
 name:"Huawei",
+category:"Loja Oficial",
 logo:"images/stores/huawei.png",
 banner:"images/stores/huawei-banner.jpg"
 },
 
 sony:{
+id:"sony",
 name:"Sony",
+category:"Loja Oficial",
 logo:"images/stores/sony.png",
 banner:"images/stores/sony-banner.jpg"
 },
 
 nike:{
+id:"nike",
 name:"Nike",
+category:"Loja Oficial",
 logo:"images/stores/nike.png",
 banner:"images/stores/nike-banner.jpg"
 },
 
 adidas:{
+id:"adidas",
 name:"Adidas",
+category:"Loja Oficial",
 logo:"images/stores/adidas.png",
 banner:"images/stores/adidas-banner.jpg"
 },
 
 puma:{
+id:"puma",
 name:"Puma",
+category:"Loja Oficial",
 logo:"images/stores/puma.png",
 banner:"images/stores/puma-banner.jpg"
 },
 
 realmadrid:{
+id:"realmadrid",
 name:"Real Madrid",
+category:"Loja Oficial",
 logo:"images/stores/realmadrid.png",
 banner:"images/stores/realmadrid-banner.jpg"
 },
 
 barcelona:{
+id:"barcelona",
 name:"FC Barcelona",
+category:"Loja Oficial",
 logo:"images/stores/barcelona.png",
 banner:"images/stores/barcelona-banner.jpg"
 },
 
 psg:{
+id:"psg",
 name:"PSG",
+category:"Loja Oficial",
 logo:"images/stores/psg.png",
 banner:"images/stores/psg-banner.jpg"
 },
 
 rolex:{
+id:"rolex",
 name:"Rolex",
+category:"Loja Oficial",
 logo:"images/stores/rolex.png",
 banner:"images/stores/rolex-banner.jpg"
 },
 
 gucci:{
+id:"gucci",
 name:"Gucci",
+category:"Loja Oficial",
 logo:"images/stores/gucci.png",
 banner:"images/stores/gucci-banner.jpg"
 },
 
 "louis-vuitton":{
+id:"louis-vuitton",
 name:"Louis Vuitton",
+category:"Loja Oficial",
 logo:"images/stores/louis-vuitton.png",
 banner:"images/stores/louis-vuitton-banner.jpg"
 }
 
 };
-
 const store = stores[storeId];
 
 if (!store) {
